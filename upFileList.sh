31e55aeb13f0e5da75a4d28f08276816fbf9e4ec
liuyao <liuyao.cool.61@gmail.com>
2024-07-28 19:50:50 +0800

    file list

--- upFileList.sh
+++ upFileList.sh
@@ -6,4 +6,4 @@ find ./ \( \
 	-o -path ./upFileList.sh \
 	-o -path ./fileList.txt \
 	-o -path ./index.html \
-\) -prune -o -type f -print > fileList.txt
+\) -prune -o -type f -print | sort -k 1 > fileList.txt
