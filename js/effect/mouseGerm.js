235c829fcbdf007060c1715bd1616de9f99a0389
liuyao <liuyao_cool@163.com>
2024-11-20 01:46:02 +0800

    mouseGerm custom

--- js/effect/mouseGerm.js
+++ js/effect/mouseGerm.js
@@ -1,4 +1,11 @@
-(function() {
+/**
+ * define to set color offset 50
+ * `const ly_germ_color_offset = 50`
+ */
+(function(color_range) {
+    if (!checkColorRange()) {
+        color_range = [[0, 0], [130, 255], [0, 150]];
+    }
     const randInt = (min, max) => {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
@@ -41,8 +48,12 @@
             let rot = randInt(0,360);
             let fadeOut = randInt(1500,2500);
             let sw = randInt(50, 200)/100;
+            let rgb = [];
+            for (let i = 0; i < color_range.length; i++) {
+                rgb[i] = randInt(color_range[i][0], color_range[i][1]);
+            }
             let p = `<g transform="rotate(${randInt(0,360)} 50 50)">
-            <path d="${d1}" stroke="rgb(0, ${randInt(130, 255)}, ${randInt(0, 150)})" stroke-width="${sw}" fill="none" stroke-linecap="round" transform="rotate(${rot} ${x} ${y + 5})">
+            <path d="${d1}" stroke="rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})" stroke-width="${sw}" fill="none" stroke-linecap="round" transform="rotate(${rot} ${x} ${y + 5})">
                 <animate attributeName="d" values="${d1}; ${d2}; ${d1}" dur="${randInt(600,1500)}ms" begin="${randInt(-1500,-600)}ms" repeatCount="indefinite" keyTimes="0;${randInt(25, 75)/100};1" keySplines="${randInt(0,100)/100} ${randInt(0,100)/100} ${randInt(0,100)/100} ${randInt(0,100)/100}; ${randInt(0,100)/100} ${randInt(0,100)/100} ${randInt(0,100)/100} ${randInt(0,100)/100}" calcMode="spline" />
                 <animateTransform attributeName="transform" attributeType="XML" type="rotate" values="${rot} ${x} ${y}; ${rot} ${x} ${y + randInt(10,25)}" dur="${fadeOut}ms" begin="0s" fill="freeze" repeatCount="none" />
                 <animate attributeName="stroke-width" values="${sw}; 0" dur="${fadeOut}ms" begin="0s" fill="freeze" repeatCount="none" keyTimes="0;1" keySplines="0.75 0 0.75 0" calcMode="spline" />
@@ -58,7 +69,26 @@
         setTimeout(() => {svg?.remove();}, 2500);
     }
 
+    function checkColorRange() {
+        if (!color_range || !Array.isArray(color_range) || color_range.length != 3) return false;
+        for (let i = 0; i < color_range.length; i++) {
+            if (!Array.isArray(color_range[i]) || color_range[i].length != 2) return false;
+            if(!isRgbNum(color_range[i][0]) || !isRgbNum(color_range[i][1])) return false;
+            ajustRange(color_range[i]);
+        }
+        return true;
+    }
+    function ajustRange(r) {
+        if (r[1] >= r[0]) return;
+        var tmp = r[0];
+        r[0] = r[1];
+        r[1] = tmp;
+    }
+    function isRgbNum(num) {
+        return num >= 0 && num < 256;
+    }
+
     // document.addEventListener("mousemove", (event) => {germ(event)});
     document.addEventListener("touchmove", (event) => germ(event));
     document.addEventListener("pointermove", (e) => germAdd(e.clientX, e.clientY));
-})()
+})(typeof ly_germ_color_offset !== 'undefined' ? ly_germ_color_offset : null)
